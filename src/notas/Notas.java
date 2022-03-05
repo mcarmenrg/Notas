4045c8b5589cd34e421a77539a7aa4c2fee82e98
mcrui <mcruizgarcia@gmail.com>
2022-03-05 12:14:49 +0100

    (Ejercicio4.Versión2)Utiliza las funciones de refactorización para sustituir el código que se encarga de especificar la calificación en función de la nota por un método que llamarás "cali", que será público, de forma que se incluyan los comentarios para la documentación si ello es posible. Muestra cómo se hace en los dos entornos. (En Eclipse y en Netbeans). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio

--- src/notas/Notas.java
+++ src/notas/Notas.java
@@ -2,49 +2,37 @@ package notas;
 
 import java.util.Scanner;
 
-/**
- * 
- */
-
-/**
- * @author mcrui
- *
- */
-
 public class Notas {
-	/**
-	 * @param args
-	 */
 
-	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);// Para pedir números por teclado
-		int nota = 0;
-		String calificacion = "";
-		System.out.print("Introduzca una nota: ");
-		nota = reader.nextInt();
+public static void main (String[] args) {
+Scanner reader = new Scanner(System.in);// Para pedir números por teclado
+int nota = 0;
+String calificacion = "";
+System.out.print("Introduzca una nota: ");
+nota = reader.nextInt();
 
-		calificacion = extracted(nota);
+calificacion = cali(nota);
 
-		System.out.println(calificacion);
-		reader.close();
-	}
+System.out.println(calificacion);
+reader.close();
+}
 
-	/**
-	 * @param num_nota
-	 * @return
-	 */
-	public static String extracted(int num_nota) {
-		String calificacion;
-		if (num_nota >= 0 && num_nota < 5)
-			calificacion = "Suspenso";
-		else if (num_nota >= 5 && num_nota < 7)
-			calificacion = "Aprobado";
-		else if (num_nota >= 7 && num_nota < 9)
-			calificacion = "Notable";
-		else if (num_nota >= 9 && num_nota <= 10)
-			calificacion = "Sobresaliente";
-		else
-			calificacion = "La nota introducida no es correcta";
-		return calificacion;
-	}
+/**
+ * @param nota
+ * @return
+ */
+public static String cali(int nota) {
+	String calificacion;
+	if (nota >= 0 && nota < 5)
+	calificacion = "Suspenso";
+	else if (nota >= 5 && nota < 7)
+	calificacion = "Aprobado";
+	else if (nota >= 7 && nota <9)
+	calificacion = "Notable";
+	else if (nota >= 9 && nota <= 10)
+	calificacion = "Sobresaliente";
+	else
+	calificacion = "La nota introducida no es correcta";
+	return calificacion;
+}
 }
